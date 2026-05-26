d5c6c715ebeba918693cf0a8542f6f52e5f4ccef
raysrego <rayannyrego@gmail.com>
2026-05-26 19:55:18 -0300

    Update route.ts

--- src/app/api/caixa/conciliacao-ia/route.ts
+++ src/app/api/caixa/conciliacao-ia/route.ts
@@ -143,6 +143,12 @@ ${JSON.stringify(dados.transacoesOFX, null, 2)}
 ## Fechamentos TFL (${dados.fechamentosTFL.length} registros)
 ${JSON.stringify(dados.fechamentosTFL, null, 2)}
 
+`## PIX Externos Unitários (Operador):
+${JSON.stringify(fechamentosCaixa.flatMap(f => f.pix_externos_unitarios), null, 2)}
+
+## Sangria TFL:
+${JSON.stringify(fechamentosTFL.map(f => ({ id: f.id, sangria: f.sangria_valor })), null, 2)}`
+
 ## Fechamentos de Caixa Operador (${dados.fechamentosCaixa.length} registros)
 ${JSON.stringify(dados.fechamentosCaixa, null, 2)}
 
